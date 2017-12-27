a4d0af6702f199c264d1b797bfddd2adba9633ba
Adrian Bustos <Adrian Bustos@192.168.1.33>
2017-12-27 15:54:30 +0100

    cliente inicio

--- src/cliente.py
+++ src/cliente.py
@@ -2,6 +2,5 @@
 #Francisco Manuel Garcia Sanchez-Belmonte
 #Fernando Galan Freire
 #Adrian Bustos Marin
-
 if __name__ == "__main__":
     print "Hello World"
--- src/player.py
+++ src/player.py
@@ -1,6 +1,21 @@
 #!/bin/bash/python3
+# -*- coding: utf-8 -*-
 #Francisco Manuel Garcia Sanchez-Belmonte
 #Fernando Galan Freire
 #Adrian Bustos Marin
-if __name__ == "__main__":
-    print "Hello World"
+#if __name__ == "__main__":
+ #   print "Hello World"
+    
+class PlayerI(drobots.Player):  
+    def win(self, current = None):
+	print("You win!")
+	current.adapter.getCommunicator().shutdown()
+
+    def lose(self, current = None):
+	print("Looser!")
+	current.adapter.getCommunicator().shutdown()
+
+    def gameAbort(self, current = None):
+	print("Game aborted")
+	current.adapter.getCommunicator().shutdown()
+
