e50b8e278cae451f1f0340090a0b14b7587083c6
OhEsPaco <fcomanuel.garcia4@alu.uclm.es>
2017-12-27 15:47:34 +0100

    Cabecero

--- src/cliente.py
+++ src/cliente.py
@@ -1,6 +1,7 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+#!/bin/bash/python3
+#Francisco Manuel Garcia Sanchez-Belmonte
+#Fernando Galan Freire
+#Adrian Bustos Marin
 
 if __name__ == "__main__":
     print "Hello World"
