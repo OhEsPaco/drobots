041c352e182972b514059e6ea876e3d08741037c
Adrian Bustos <Adrian Bustos@DESKTOP-M04B9NS>
2018-01-19 19:36:20 +0100

    Container.py

--- src/container.py
+++ src/container.py
@@ -2,5 +2,28 @@
 #Francisco Manuel Garcia Sanchez-Belmonte
 #Fernando Galan Freire
 #Adrian Bustos Marin
-if __name__ == "__main__":
-    print "Hello World"
+import sys
+import Ice
+Ice.loadSlice('container.ice')
+import Services
+
+class Container(Services.Container):
+    def __init__(self):
+        self.proxies = dict()
+
+    def link(self, key, proxy, current=None):
+        if key in self.proxies:
+            raise Services.AlreadyExists(key)
+        print("link: {0} -> {1}".format(key, proxy))
+        self.proxies[key] = proxy
+    
+    def unlink(self, key, current=None):
+        if not key in self.proxies:
+            raise Services.NoSuchKey(key)
+        del self.proxies[key]
+    
+    def list(self, current=None):
+        return self.proxies, list(self.proxies.keys())
+
+    def listR(self, current=None):
+        return self.proxies
